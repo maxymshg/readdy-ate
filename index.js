e68f9ae130015a1a2bb3d0a2fd822898db771efa
Maksym Shekhovets <maxymshg@gmail.com>
2020-11-14 12:22:18 +0200

    Update extract function
    - site to text
    - put file on s3

--- index.js
+++ index.js
@@ -1,8 +1,61 @@
+const { Readability } = require("@mozilla/readability");
+const axios = require('axios').default;
+const JSDOM = require('jsdom').JSDOM;
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+  console.log('request: ' + JSON.stringify(event));
+  let body;
+  if (event.body) {
+    body = JSON.parse(event.body);
+  }
+
+  let articleUrl;
+  if (body) {
+    articleUrl = body.url;
+  }
+
+  let readerView;
+  let response;
+  try {
+    if (articleUrl) {
+      console.log('url: ' + articleUrl);
+      const articleSite = await axios.get(articleUrl);
+      const vdom = new JSDOM(articleSite.data)
+      const reader = new Readability(vdom.window.document);
+      readerView = reader.parse();
+    }
+
+    const destparams = {
+      Bucket: 'readdy-ate-content',
+      Key: readerView.title + '.txt',
+      Body: readerView.textContent,
+      ContentType: 'text',
     };
+
+    await s3.putObject(destparams).promise();
+  } catch (error) {
+    console.log(error);
+    response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          data: {
+            msg: 'Error: ' + error,
+          },
+        }),
+      };
     return response;
+  }
+
+  response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: {
+        msg: 'Processing is complete: ' + readerView.title + '.txt',
+      },
+    }),
+  };
+
+  return response;
 };
